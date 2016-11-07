a8138738cecb2aeab4d3f1c22e99ba1d1fc2e59a
Will Reichert <willr3@gmail.com>
2016-11-07 13:42:31 -0600

    remove Override for AMQBuffer methods which return another buffer

--- artemis-commons/src/main/java/org/apache/activemq/artemis/api/core/ActiveMQBuffers.java
+++ artemis-commons/src/main/java/org/apache/activemq/artemis/api/core/ActiveMQBuffers.java
@@ -37,44 +37,40 @@ public final class ActiveMQBuffers {
          super(buffer,bool);//always releasable
       }
 
-      @Override
-      public ActiveMQBuffer copy() {
-         return new AMQBuffer(buffer.copy(), releasable);
-      }
-      @Override
-      public ActiveMQBuffer copy(final int index, final int length) {
-         return new AMQBuffer(buffer.copy(index, length), releasable);
-      }
-      @Override
-      public ActiveMQBuffer duplicate() {
-         return new AMQBuffer(buffer.duplicate(), releasable);
-      }
-      @Override
-      public ActiveMQBuffer readBytes(final int length) {
-         return new AMQBuffer(buffer.readBytes(length), releasable);
-      }
-      @Override
-      public ActiveMQBuffer readSlice(final int length) {
-         return new AMQBuffer(buffer.readSlice(length), releasable);
-      }
-      @Override
-      public ActiveMQBuffer slice() {
-         return new AMQBuffer(buffer.slice(), releasable);
-      }
-      @Override
-      public ActiveMQBuffer slice(final int index, final int length) {
-         return new AMQBuffer(buffer.slice(index, length), releasable);
-      }
+//      @Override
+//      public ActiveMQBuffer copy() {
+//         return new AMQBuffer(buffer.copy(), releasable);
+//      }
+//      @Override
+//      public ActiveMQBuffer copy(final int index, final int length) {
+//         return new AMQBuffer(buffer.copy(index, length), releasable);
+//      }
+//      @Override
+//      public ActiveMQBuffer duplicate() {
+//         return new AMQBuffer(buffer.duplicate(), releasable);
+//      }
+//      @Override
+//      public ActiveMQBuffer readBytes(final int length) {
+//         return new AMQBuffer(buffer.readBytes(length), releasable);
+//      }
+//      @Override
+//      public ActiveMQBuffer readSlice(final int length) {
+//         return new AMQBuffer(buffer.readSlice(length), releasable);
+//      }
+//      @Override
+//      public ActiveMQBuffer slice() {
+//         return new AMQBuffer(buffer.slice(), releasable);
+//      }
+//      @Override
+//      public ActiveMQBuffer slice(final int index, final int length) {
+//         return new AMQBuffer(buffer.slice(index, length), releasable);
+//      }
       @Override
       public void retain(){
          buffer.retain();
       }
       @Override
-      public void release(){
-         //buffer.capacity();
-         buffer.release();
-
-      }
+      public void release(){ buffer.release(); }
    }
 
 
